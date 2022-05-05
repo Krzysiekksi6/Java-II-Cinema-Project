4240179c039112bd02760ca60249796caf0969b1
Krzysiek <krzysiekksi6@gmail.com>
2022-05-05 14:03:15 +0200

    get Date String

--- src/main/java/pl/cinema/controller/ReservationPaneController.java
+++ src/main/java/pl/cinema/controller/ReservationPaneController.java
@@ -4,13 +4,27 @@ import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
+import javafx.scene.control.DatePicker;
+import javafx.scene.control.Label;
 import pl.cinema.app.Main;
 
 import java.io.IOException;
+import java.time.LocalDate;
 
 public class ReservationPaneController {
     @FXML
     private Button backButton;
+
+    @FXML
+    private DatePicker datePicker;
+
+    @FXML
+    private Label dataLabel;
+
+    public void getDate(ActionEvent event) {
+        LocalDate myDate = datePicker.getValue();
+        dataLabel.setText(myDate.toString());
+    }
 
     public void initialize(){
         backButton.setOnAction(new EventHandler<ActionEvent>() {
@@ -23,6 +37,8 @@ public class ReservationPaneController {
                 }
             }
         });
+
+
     }
 
 }
